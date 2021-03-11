457f21212a12f34eab62a7d289a1fe3e02235821
Rodrigo <rodrigorael53@gmail.com>
2021-03-11 16:09:04 -0300

    add eslint, prettier and editorconfig

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // coloquei que eu quero ; no final das linhas
+  semi: true,
+  // ccoloque que quero utilizar aspas simples('')
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};

2efad40eac45a9c37417612c42eee3e95a7f059a
Rodrigo <rodrigorael53@gmail.com>
2021-03-12 01:30:13 -0300

    separating the home page into a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-
 import styled from 'styled-components';
+
 import db from '../../db.json';
+
+import { Widget } from '../components/Widget';
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -19,58 +21,6 @@ export const QuizContainer = styled.div`
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
-  }
-`;
-
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1,
-  h2,
-  h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`;
-
-// aqui eu estou estilizando o conteúdo dentro do Widget
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  * {
-    margin: 0;
-  }
-`;
-
-// aqui eu estou estilizando o conteúdo dentro do Widget
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
   }
 `;
 
